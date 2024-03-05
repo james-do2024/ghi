d4c6a675f6fa1dfdd1e998e8a3eeda0ee72ecda8
James Taylor <james@mailhaven.com>
2024-03-05 17:26:21 -0600

    Added early REST client code.

---
+++ client/rest.go
@@ -0,0 +1,71 @@
+/*
+ *   Because we are leveraging the GitHub client module, much
+ *   of the heavy lifting has already been done. However, it
+ *   would be nice to be able to override some defaults with
+ *   environment variables and set the auth token that way.
+ *
+ *   A thin wrapper can achieve this.
+ */
+
+package client
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"regexp"
+
+	"github.com/google/go-github/v60/github"
+	"github.com/james-do2024/ghi/config"
+)
+
+func NewRestClient() (*github.Client, error) {
+	client := github.NewClient(nil)
+	if err := applyEnvVars(client); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func applyEnvVars(c *github.Client) error {
+	if env, base, err := config.GetEnvIfSet("defaultBaseURL"); err != nil {
+		return err
+	} else if base != "" {
+		// It is possible for the URL in the env var to be invalid - check it here
+		parsedURL, err := url.ParseRequestURI(base)
+		if err != nil {
+			// Log the problem, but carry on with module's default value
+			log.Printf("malformed url in env var %s: %s\n", env, base)
+		} else {
+			c.BaseURL = parsedURL
+		}
+	}
+
+	if _, agent, err := config.GetEnvIfSet("defaultUserAgent"); err != nil {
+		return err
+	} else if agent != "" {
+		c.UserAgent = agent
+	}
+
+	// For simplicity, require token and treat its absence as fatal
+	env, token, err := config.GetEnvIfSet("authToken")
+	if err != nil {
+		return err
+	} else if token == "" {
+		return fmt.Errorf("auth token not set or empty in %s", env)
+	}
+
+	if !isPATValid(token) {
+		return fmt.Errorf("supplied token does not appear to be a valid GitHub PAT")
+	}
+
+	c = c.WithAuthToken(token)
+	return nil
+}
+
+// Validates structure and size only
+func isPATValid(token string) bool {
+	tokenRx := regexp.MustCompile(`^github_pat_[a-zA-Z0-9_]{28,243}$`)
+	return tokenRx.MatchString(token)
+}
--- go.mod
+++ go.mod
@@ -2,9 +2,15 @@ module github.com/james-do2024/ghi
 
 go 1.21.6
 
-require github.com/spf13/cobra v1.8.0
+require (
+	github.com/mattn/go-isatty v0.0.20
+	github.com/spf13/cobra v1.8.0
+)
 
 require (
+	github.com/google/go-github/v60 v60.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 )
