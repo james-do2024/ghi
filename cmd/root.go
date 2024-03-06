84abf5cf2c1f521330d2467d36acfb880e8b5d22
James Taylor <james@mailhaven.com>
2024-03-06 08:19:48 -0600

    Minimal functionality for view command, including arg handling.

--- cmd/root.go
+++ cmd/root.go
@@ -4,8 +4,11 @@ Copyright © 2024 James Taylor <james.taylor@fastmail.com>
 package cmd
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/james-do2024/ghi/config"
 	"github.com/spf13/cobra"
@@ -34,4 +37,34 @@ func Execute() {
 
 func init() {
 	rootCmd.Flags().BoolP("debug", "d", false, "Run in debug mode")
+}
+
+func handleOneArg(arg string) (owner, repo, path string, err error) {
+	// Allow users to enter: ghi <view, explore> owner/repo [path]
+	baseRegex := regexp.MustCompile(`^[\w-]+/[\w-]+$`)
+
+	// Another pattern is: ghi <view, explore> owner/repo/path
+	oneArgRegex := regexp.MustCompile(`^[\w-]+/[\w-]+(?:/[\w-./]+)$`)
+
+	if oneArgRegex.MatchString(arg) {
+		splitArg := strings.SplitN(arg, "/", 3)
+		owner, repo, path = splitArg[0], splitArg[1], splitArg[2]
+		return owner, repo, path, nil
+	} else if baseRegex.MatchString(arg) {
+		splitArg := strings.SplitN(arg, "/", 2)
+		owner, repo = splitArg[0], splitArg[1]
+		return owner, repo, "", nil
+	}
+	return "", "", "", fmt.Errorf("Input does not match `owner/repo` pattern: %s", arg)
+}
+
+func handleTwoArgs(arg1, arg2 string) (owner, repo, path string, err error) {
+	baseRegex := regexp.MustCompile(`^[\w-]+/[\w-]+$`)
+	if baseRegex.MatchString(arg1) {
+		splitArg := strings.SplitN(arg1, "/", 2)
+		owner, repo = splitArg[0], splitArg[1]
+		path = arg2
+		return owner, repo, path, nil
+	}
+	return "", "", "", fmt.Errorf("Input does not match `owner/repo` pattern: %s", arg1)
 }
--- cmd/view.go
+++ cmd/view.go
@@ -4,25 +4,73 @@ Copyright © 2024 James Taylor <james.taylor@fastmail.com>
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"os"
 
+	"github.com/james-do2024/ghi/client"
+	"github.com/james-do2024/ghi/config"
 	"github.com/spf13/cobra"
 )
 
 var viewCmd = &cobra.Command{
 	Use:   "view",
-	Short: "Non-interactively poll and display a GitHub repository",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Non-interactively poll and display GitHub repository content",
+	Long: `The view command displays the requested contents simply and then exits,
+foregoing syntax highlighting or any further interactive use.`,
+	Args: cobra.MaximumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("view called")
+		cmdMain(args)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(viewCmd)
+}
+
+func cmdMain(args []string) {
+	var owner, repo, path string
+	var err error
+
+	if len(args) == 1 {
+		owner, repo, path, err = handleOneArg(args[0])
+	} else {
+		owner, repo, path, err = handleTwoArgs(args[0], args[1])
+	}
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(config.ExitErr)
+	}
+	runRequest(owner, repo, path)
+}
+
+func runRequest(owner, repo, path string) error {
+	ctx := context.Background()
+	c, err := client.NewRestClient()
+	if err != nil {
+		return err
+	}
+
+	req := &client.RestRequest{
+		GitClient: c,
+		Ctx:       ctx,
+	}
+
+	file, dir, err := req.GetContent(owner, repo, path)
+	if err != nil {
+		return err
+	}
+
+	if file != nil {
+		fmt.Print(*file)
+	} else {
+		for _, entry := range dir {
+			if *entry.Type == "dir" {
+				*entry.Name += "/" // Simple way to differentiate directories in repo view
+			}
+			fmt.Println(*entry.Name)
+		}
+	}
+	return nil
 }
