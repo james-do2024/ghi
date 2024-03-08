127e1c3e1c9b87ef4de732530d9f73f07e707c24
James Taylor <james@mailhaven.com>
2024-03-07 20:38:00 -0600

    Single import, keep it on one line.

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ Copyright © 2024 James Taylor <james.taylor@fastmail.com>
 */
 package main
 
-import (
-	"github.com/james-do2024/ghi/cmd"
-)
+import "github.com/james-do2024/ghi/cmd"
 
 func main() {
 	cmd.Execute()
